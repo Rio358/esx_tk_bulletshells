94a8cd615d5d9ca697bd76cccc25b0e40d0fab20
TuKeh <tukehhz@gmail.com>
2021-07-20 01:52:54 +0300

    update

--- config.lua
+++ config.lua
@@ -8,13 +8,11 @@ Config.WeaponToSearch = 'WEAPON_FLASHLIGHT'
 -- Time in milliseconds
 Config.SearchTime = 10000
 
--- Time in seconds
-Config.DisappearTime = 1800
+-- Time in minutes (Last number)
+Config.DisappearTime = 1000*60*60
 
 Config.SeeDistance = 5
 Config.InspectDistance = 2
 Config.DuplicateDistance = 1.5
-
-Config.EnableDuplicate = true
 
 
